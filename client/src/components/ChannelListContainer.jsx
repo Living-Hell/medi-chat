eb39c76af7fb0486704111e2fc1498da165bff3a
Shushant Kumar <s.living.hell@gmail.com>
2022-08-27 01:32:05 +0530

    Fixed the issue with login & signup

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -5,7 +5,9 @@ import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import HospitalIcon from '../assets/hospital.png';
 import LogoutIcon from '../assets/logout.png';
 
-const Sidebar = () => (
+const cookies = new Cookies();
+
+const Sidebar = ({ logout }) => (
     <div className = "channel-list__sidebar">
         <div className = "channel-list__sidebar__icon1">
             <div className = "icon1__inner">
@@ -13,7 +15,7 @@ const Sidebar = () => (
             </div>
         </div>
         <div className = "channel-list__sidebar__icon2">
-            <div className = "icon1__inner">
+            <div className = "icon1__inner" onClick={logout}>
                 <img src = {LogoutIcon} alt="Logout" width="30" />
             </div>
         </div>
@@ -27,9 +29,23 @@ const CompanyHeader = () => (
 );
 
 const ChannelListContainer = () => {
+
+    const logout = () => {
+        cookies.remove("token");
+        cookies.remove('userId');
+        cookies.remove('username');
+        cookies.remove('fullName');
+        cookies.remove('avatarURL');
+        cookies.remove('hashedPassword');
+        cookies.remove('phoneNumber');
+
+        window.location.reload();
+
+    }
+
   return (
     <>
-        <Sidebar />
+        <Sidebar logout = {logout}/>
         <div className = "channel-list__list__wrapper">
             <CompanyHeader />
             <ChannelSearch />
