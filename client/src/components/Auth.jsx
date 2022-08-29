3437ecf12ec8cc220b73da1ed671997ce8dfcdaa
Shushant Kumar <s.living.hell@gmail.com>
2022-08-29 22:59:49 +0530

    Happy Messaging :)

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -29,7 +29,7 @@ const Auth = () => {
         const { username, password, phoneNumber, avatarURL } = form;
 
         //const URL = 'http://localhost:5000/auth';
-        const URL = 'https://medi-chatt.herokuapp.com//auth';
+        const URL = 'https://medi-chatt.herokuapp.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
